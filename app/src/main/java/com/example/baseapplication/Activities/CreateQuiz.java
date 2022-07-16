2e428a367b941b225d6e02733b940170401bee95
Umang Singh <Umang2singh@gmail.com>
2022-07-16 13:33:51 +0530

    SOme Changes

--- app/src/main/java/com/example/baseapplication/Activities/CreateQuiz.java
+++ app/src/main/java/com/example/baseapplication/Activities/CreateQuiz.java
@@ -1,25 +1,63 @@
 package com.example.baseapplication.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.baseapplication.Adapter.CreateQuestionDialog;
+import com.example.baseapplication.Model.Questions;
 import com.example.baseapplication.R;
 
-public class CreateQuiz extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CreateQuiz extends AppCompatActivity implements CreateQuestionDialog.DialogListener {
+
+    private Button submit,accept,cancel;
+    private ArrayList<Questions> list;
+    private TextView emptyrv;
+    private EditText title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quiz);
+
+        emptyrv = findViewById(R.id.no_item_in_rv);
+        submit = findViewById(R.id.submit);
+        title = findViewById(R.id.q_title);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add quiz to firebase
+
+
+            }
+        });
+
+
+
+
     }
 
     public void addQuestions(View view) {
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.create_qna);
+        CreateQuestionDialog dialog = new CreateQuestionDialog();
+        dialog.show(getSupportFragmentManager(),"Add Question");
+
+    }
+
+    @Override
+    public void addQuestion(String title, String opA, String opB, String opC, String opD, String ans) {
+        list.add(new Questions(title,opA,opB,opC,opD,ans));
     }
 }
+
---
+++ app/src/main/java/com/example/baseapplication/Adapter/CreateQuestionDialog.java
@@ -0,0 +1,129 @@
+package com.example.baseapplication.Adapter;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.example.baseapplication.R;
+
+
+public class CreateQuestionDialog extends AppCompatDialogFragment {
+    private EditText q,a,b,c,d;
+    private DialogListener listener;
+    private ImageButton accept,cancel;
+    private CheckBox oa,ob,oc,od;
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//        //Personalizamos
+//
+//        Resources res = getResources();
+//
+//        //Buttons
+//        Button positive_button =  ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_POSITIVE);
+//        positive_button.setBackground(res.getDrawable(R.drawable.btn_selector_dialog));
+//
+//        Button negative_button =  ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_NEGATIVE);
+//        negative_button.setBackground(res.getDrawable(R.drawable.btn_selector_dialog));
+//
+//        int color = Color.parseColor("#304f5a");
+//        }
+//    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.create_qna, null);
+
+//        accept = view.findViewById(R.id.accept);
+//        cancel = view.findViewById(R.id.cancel);
+        q = view.findViewById(R.id.question);
+        a = view.findViewById(R.id.opA);
+        b = view.findViewById(R.id.opB);
+        c = view.findViewById(R.id.opC);
+        d = view.findViewById(R.id.opD);
+        oa = view.findViewById(R.id.optionA);
+        ob = view.findViewById(R.id.optionB);
+        oc = view.findViewById(R.id.optionC);
+        od = view.findViewById(R.id.optionD);
+
+        builder.setView(view).
+                setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String ans = "";
+
+                        if(oa.isChecked()) ans+="a";
+                        if(ob.isChecked()) ans+="b";
+                        if(oc.isChecked()) ans+="c";
+                        if(od.isChecked()) ans+="d";
+
+                        listener.addQuestion(q.getText().toString(),a.getText().toString(),b.getText().toString(),c.getText().toString(),d.getText().toString(),ans);
+
+
+                    }
+                });
+
+//        accept.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String ans = "";
+//
+//                if(oa.isChecked()) ans+="a";
+//                if(ob.isChecked()) ans+="b";
+//                if(oc.isChecked()) ans+="c";
+//                if(od.isChecked()) ans+="d";
+//
+//                listener.addQuestion(q.getText().toString(),a.getText().toString(),b.getText().toString(),c.getText().toString(),d.getText().toString(),ans);
+//
+//
+//            }
+//        });
+//
+//        cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (DialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    "must implement CreateQuestionDialog");
+        }
+    }
+
+    public interface DialogListener {
+        void addQuestion(String title, String opA, String opB, String opC, String opD, String ans);
+    }
+}
